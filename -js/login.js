c68d29cef0cfb84802a54974dba385beb9ec0a97
Edley Teixeira <edleyteixeira@hotmail.com>
2020-12-24 07:44:50 -0300

    Login
    Pagina login concluida html, csse js

---
+++ -js/login.js
@@ -0,0 +1,12 @@
+function mostrar(){
+
+    document.getElementById("floatingPassword").type = "text";
+    document.getElementById("img").src = "https://icons.getbootstrap.com/icons/eye-fill.svg";
+    
+    
+}
+function ocultar(){
+
+    document.getElementById("floatingPassword").type = "password";
+    document.getElementById("img").src = "https://icons.getbootstrap.com/icons/eye-slash-fill.svg";
+}
